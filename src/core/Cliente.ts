fe444bfb38de56a5c6ea95a47f8f1fc4fee6b33e
GMpatrick <patrickalvessc@gmail.com>
2022-11-24 09:43:43 -0300

    Adição de componentes e configuração do projeto

---
+++ src/core/Cliente.ts
@@ -0,0 +1,29 @@
+/* Classe que representa o cliente */
+
+export default class Cliente{
+    private id: string
+    private nome: string
+    private idade: number
+
+    constructor(nome: string, idade: number, id:string = null){
+        this.nome = nome
+        this.idade = idade
+        this.id = id
+    }
+
+    static vazio(){
+        return new Cliente('', 0)
+    }
+
+    get getId(){
+        return this.id
+    }
+
+    get getNome(){
+        return this.nome
+    }
+
+    get getIdade(){
+        return this.idade
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
+import Botao from "../components/botao";
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
     {/* <h1 className={`flex h-screen justify-center 
       items-center bg-gradient-to-r from bg-purple-500 to-blue-600 `}>
       <span className="text-4xl ">Texto</span>
     </h1> exemplo de como deve ser*/}
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Patrick', 19, '2'),
+    new Cliente('Pai', 54, '3'),
+    new Cliente('Mãe', 52, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+    console.log(cliente.getNome)
+  }
+
+  function clienteExcluido(cliente: Cliente){
+    console.log(cliente.getId)
+  }
+  
   return (
     <div className={`
         flex justify-center items-center h-screen
@@ -12,7 +31,13 @@ export default function Home() {
         text-white 
       `}>
         <Layout titulo="Cadastro Simples">
-            <span>Conteudo</span>           
+          <div className="flex justify-end">
+            <Botao className="mb-4" cor="green">Novo Cliente</Botao>
+          </div>
+            <Tabela clientes={clientes} 
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            /> 
         </Layout>
     </div> 
   )
